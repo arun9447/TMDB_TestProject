dacb62d5ddf336826a3a012d93a922975ea79889
Arun <arunkumar@changejar.in>
2024-12-23 20:27:49 +0530

    Project created

---
+++ TMDB_TestProject/TMDB_TestProject/TMDB_TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_TestProjectApp.swift
+//  TMDB_TestProject
+//
+//  Created by Arunkumar K M on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
